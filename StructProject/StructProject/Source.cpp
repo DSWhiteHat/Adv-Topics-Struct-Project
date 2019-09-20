7cea4f30b3c45c15e21c9f7aceb029f477291fde
DSWhiteHat <33768979+DSWhiteHat@users.noreply.github.com>
2019-09-20 13:36:32 -0400

    Start

---
+++ StructProject/StructProject/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+struct character
+{
+	char name[50];
+	int health;
+	int strength;
+	int attack;
+	int defense;
+	int mana;
+	int stamina;
+};
+
+int main()
+{
+
+}
